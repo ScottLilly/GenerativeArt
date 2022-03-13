3b249dbe222e1df243177205dd79ddbd14f4423f
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-13 18:33:22 -0500

    Add project files.

---
+++ GenerativeArt.WPF/GenerativeArt.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace GenerativeArt.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateRectangles_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
