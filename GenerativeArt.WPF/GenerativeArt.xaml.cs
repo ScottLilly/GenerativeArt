2abe4f7e67dac436071e35fb3816e47d26da250f
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-13 19:34:17 -0500

    Drawing random-colored rectangles

--- GenerativeArt.WPF/GenerativeArt.xaml.cs
+++ GenerativeArt.WPF/GenerativeArt.xaml.cs
@@ -1,22 +1,68 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using GenerativeArt.Core;
 
-namespace GenerativeArt.WPF
+namespace GenerativeArt.WPF;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void CreateRectangles_OnClick(object sender, RoutedEventArgs e)
+    private void CreateRectangles_OnClick(object sender, RoutedEventArgs e)
+    {
+        for (int i = 0; i < 50; i++)
         {
+            Rectangle rect = new Rectangle
+            {
+                Height = RngCreator.GetNumberBetween(10, 50),
+                Width = RngCreator.GetNumberBetween(50, 150)
+            };
 
-        }
+            SolidColorBrush rectangleFillBrush = RandomBrush();
 
-        private void Exit_OnClick(object sender, RoutedEventArgs e)
-        {
-            Close();
+            SolidColorBrush borderFillBrush = new SolidColorBrush();
+            borderFillBrush.Color = Colors.Black;
+
+            rect.StrokeThickness = 2;
+            rect.Stroke = borderFillBrush;
+            rect.Fill = rectangleFillBrush;
+
+            rect.RenderTransformOrigin = new Point(0.5, 0.5);
+            rect.RenderTransform = new RotateTransform(RngCreator.GetNumberBetween(0, 359));
+
+            ShapeCanvas.Children.Add(rect);
+
+            var maxRectangleDimension = Math.Max(rect.Height, rect.Width);
+
+            int max =
+                (int)Math.Max(
+                    (ShapeCanvas.Height - maxRectangleDimension),
+                    (ShapeCanvas.Width - maxRectangleDimension));
+
+            Canvas.SetTop(rect, RngCreator.GetNumberBetween(1 + (int)(maxRectangleDimension / 2), max));
+            Canvas.SetLeft(rect, RngCreator.GetNumberBetween(1 + (int)(maxRectangleDimension / 2), max));
         }
+    }
+
+    private void Exit_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private SolidColorBrush RandomBrush()
+    {
+        return new SolidColorBrush(Color.FromArgb(255,
+            RandomByteValue(), RandomByteValue(), RandomByteValue()));
+    }
+
+    private static byte RandomByteValue()
+    {
+        return (byte)RngCreator.GetNumberBetween(0, byte.MaxValue + 1);
     }
 }
