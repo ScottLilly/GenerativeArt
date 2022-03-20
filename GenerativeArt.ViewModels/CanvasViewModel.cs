9cc01b6981abbb350100a67db19c32ff31c4d3da
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-20 15:13:08 -0500

    Limit number of shapes on canvas, based on ViewModel property

--- GenerativeArt.ViewModels/CanvasViewModel.cs
+++ GenerativeArt.ViewModels/CanvasViewModel.cs
@@ -13,11 +13,13 @@ public class CanvasViewModel
     public int Width { get; set; }
     public ObservableCollection<IShape> Shapes { get; } =
         new ObservableCollection<IShape>();
+    public int MaximumNumberOfShapesOnCanvas { get; set; }
 
     public CanvasViewModel()
     {
         Height = 800;
         Width = 800;
+        MaximumNumberOfShapesOnCanvas = 255;
 
         //_rectangleGenerator =
         //    ShapeGeneratorFactory.GetRectangleGenerator(ShapeGeneratorFactory.GeneratorType.Random, Height, Width);
@@ -35,10 +37,27 @@ public class CanvasViewModel
     public void AddRectangle()
     {
         Shapes.Add(_rectangleGenerator.GetRectangle());
+
+        RemoveOldShapes();
     }
 
     public void AddEllipse()
     {
         Shapes.Add(_ellipseGenerator.GetEllipse());
+
+        RemoveOldShapes();
+    }
+
+    private void RemoveOldShapes()
+    {
+        if (MaximumNumberOfShapesOnCanvas < 1)
+        {
+            return;
+        }
+
+        if (Shapes.Count > MaximumNumberOfShapesOnCanvas)
+        {
+            Shapes.RemoveAt(0);
+        }
     }
 }
